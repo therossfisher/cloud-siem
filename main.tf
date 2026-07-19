7a07f7aa80631752ba0c1225442d699518a58abc
therossfisher <rosscodes@proton.me>
2026-07-18 19:43:13 -0700

    add S3 backend for remote state, Phase 9

--- main.tf
+++ main.tf
@@ -5,6 +5,14 @@ terraform {
       version = "~> 5.0"
     }
   }
+ 
+  backend "s3" {
+  bucket       = "cloud-siem-tfstate-rossfisher-is-cool"
+  key          = "cloud-siem/terraform.tfstate"
+  region       = "us-east-1"
+  encrypt      = true
+  use_lockfile = true
+ }
 }
 
 provider "aws" {
